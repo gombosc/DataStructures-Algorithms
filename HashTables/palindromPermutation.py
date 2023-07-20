88770c1f104e4cdcea44f071b9202ac55158e7b2
Cosmin <cosmingombos@gmail.com>
2023-07-20 21:44:39 +0200

    added permutation exercise

---
+++ HashTables/palindromPermutation.py
@@ -0,0 +1,23 @@
+word = 'civic'
+
+def palindromPermutation(word):
+    word = word.lower()
+    word = word.replace(" ", "")
+    
+    unpaired_chars = set()
+
+    for char in word:
+        if char in unpaired_chars:
+            unpaired_chars.remove(char)
+        else:
+            unpaired_chars.add(char)
+    
+    if(len(unpaired_chars) > 1):
+        print("Not a palindrome permutation")
+        return False
+    else:
+        print("Word is a Palindrome permutation")
+        return True
+    
+
+palindromPermutation(word)
